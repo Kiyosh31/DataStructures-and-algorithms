bdf292e42da3846765503ecca676c5dc13a210e1
David Garcia <david_5043@hotmail.com>
2022-06-10 11:08:59 -0500

    Added comments to Stack

--- data-structures/stack/stack.js
+++ data-structures/stack/stack.js
@@ -4,11 +4,13 @@ class Stack {
     this._items = {}
   }
 
+  // add an element to the top of the stack
   push(element) {
     this._items[this._count] = element
     this._count++
   }
 
+  // returns the element at the top of the stack (removing it)
   pop() {
     if (this.isEmpty()) {
       return undefined
@@ -20,6 +22,7 @@ class Stack {
     return result
   }
 
+  // returns the element at the top of the stack (without removing it)
   peek() {
     if (this.isEmpty()) {
       return undefined
@@ -28,19 +31,23 @@ class Stack {
     return this._items[this._count - 1]
   }
 
+  // returns True/False if stack is empty
   isEmpty() {
     return this._count === 0
   }
 
+  // deletes all the elements in stack
   clear() {
     this._count = 0
     this._items = {}
   }
 
+  // returns the size of the stack
   size() {
     return this._count
   }
 
+  // returns all the elements in stack in string form
   toString() {
     if (this.isEmpty()) {
       return ''
